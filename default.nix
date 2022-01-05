3d04f9e47ebf6ed6a105e6cf64724b84d0432c81
Alex Ameen <alex.ameen.tx@gmail.com>
2022-01-05 13:21:35 -0600

    add ak-core to runtime search PATH

--- default.nix
+++ default.nix
@@ -1,13 +1,15 @@
 { stdenv, lib, autoreconfHook, ak-core, graphviz, makeWrapper }:
-stdenv.mkDerivation {
+let
+  buildInputs = [graphviz ak-core];
+in stdenv.mkDerivation {
   pname = "depgraph";
   version = "0.1.0";
   src = ./.;
   nativeBuildInputs = [autoreconfHook makeWrapper];
-  buildInputs = [graphviz];
+  inherit buildInputs;
   preFixup = ''
     for f in $( find $out/bin/ -type f -executable ); do
-      wrapProgram "$f" --prefix PATH : "${lib.makeBinPath [graphviz]}"
+      wrapProgram "$f" --prefix PATH : "${lib.makeBinPath buildInputs}"
     done
   '';
   meta.license = lib.licenses.bsd3;
